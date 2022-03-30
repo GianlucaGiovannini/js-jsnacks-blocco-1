4726cbd0a4020e3975a888888372db2e58c9883c
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-30 10:33:21 +0200

    live_1 add more condition

--- live_1/main.js
+++ live_1/main.js
@@ -14,11 +14,14 @@ if (userNumber1 > userNumber2) {
     stamp = `il numero ${userNumber1} è maggiore del numero ${userNumber2}`;
 } else if (userNumber1 < userNumber2) {
     stamp = `il numero ${userNumber2} è maggiore del numero ${userNumber1}`;
-} else {
+} else if (userNumber1 == userNumber2) {
     stamp = `il numero ${userNumber1} è uguale al numero ${userNumber2}`;
+} else {
+    stamp = "errore, inserisci due numeri"
 }
 
 console.log(stamp)
+
 const stampa_a_schermo = document.querySelector("h1")
 
 stampa_a_schermo.innerHTML = stamp
