923a1f38420498c3a37e0064ffdee5db3ef2fd3f
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-30 12:51:38 +0200

    snack_5 Commit

---
+++ snack_5/main.js
@@ -0,0 +1,22 @@
+/* 
+SNACK 5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+*/
+
+const numeriDispari = [];
+
+
+for (let i = 0; i < 6; i++) {
+    let numeroGiocatore = Number(prompt("Inserisci un numero"));
+    console.log(numeroGiocatore)
+
+
+    if (numeroGiocatore % 2 != 0) {
+        numeriDispari.push(numeroGiocatore)
+    }
+
+}
+
+console.log(numeriDispari)
