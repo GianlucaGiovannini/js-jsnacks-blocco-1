627c1b131ec0102f4312529b790d923caf277f54
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-30 15:20:13 +0200

    snack_9 commit

--- snack_8/main.js
+++ snack_8/main.js
@@ -10,7 +10,9 @@ let somma = 0;
 
 for (let i = 0; i < numeroUtente.length; i++) {
     somma = somma + parseInt(numeroUtente[i]);
+    console.log(somma)
 }
 
+const stampa_a_schermo = document.querySelector("h1")
 
-console.log(somma)
+stampa_a_schermo.innerHTML = `la somma di tutte le cifre che compongono il numero che è scelto è ${somma}`
---
+++ snack_9/main.js
@@ -0,0 +1,25 @@
+/* 
+SNACK 9
+calcola la somma e la media dei primi 10 numeri
+*/
+
+const numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log(numeri);
+
+let somma = 0;
+
+for (let i = 0; i < 10; i++) {
+    somma = somma + parseInt(numeri[i]);
+    console.log(somma)
+
+}
+
+const media = somma / 10
+console.log(media);
+
+const stampaSomma = document.querySelector("h1")
+stampaSomma.innerHTML = `la somma dei primi 10 numeri è ${somma}`
+
+const stampaMedia = document.querySelector("h2")
+stampaMedia.innerHTML = `la media della somma dei primi 10 numeri diviso i 10 numeri è ${media}`
