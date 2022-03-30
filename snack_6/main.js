73d6e70b9f98720ce181ceec480ab76d74df6c60
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-30 15:46:45 +0200

    snack_6 aggiunta nuova soluzione

--- snack_6/main.js
+++ snack_6/main.js
@@ -2,13 +2,27 @@
 SNACK 6
 Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente
 */
+// chiedi all'utente un numero 
+const numeroUtente = prompt("Inserisci un numero");
 
-const numeroUtente = Number(prompt("Inserisci un numero"));
-
-
-const numeroElevato = Math.pow(numeroUtente, 3)
+// calcola il cubo del numero che ha scelto 
+const numeroElevato = parseInt(Math.pow(numeroUtente, 3))
 
 console.log(numeroElevato)
+
+// calcola il cubo dei numeri che l'utente ha digitato quindi se digita 5 bisogna fare la potenza di 1 - 2 - 3 - 4 - 5 
+
+for (let i = 1; i <= numeroUtente; i++) {
+    let cubi = parseInt(Math.pow(i, 3))
+
+    console.log(cubi)
+}
+
+
+
+
+
+
 
 const stampa_a_schermo = document.querySelector("h1")
 
