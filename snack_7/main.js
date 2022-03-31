e2500ed6d885223266d6eaa87ec936c234871e41
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-31 10:05:32 +0200

    snack_7 add second solution

--- snack_7/main.js
+++ snack_7/main.js
@@ -5,7 +5,12 @@ Stampa le potenze di 2 fino a 1000
 
 const numeroDaElevare = 2;
 
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 10; i++) {
     let numeroElevato = Math.pow(numeroDaElevare, i);
     console.log(numeroElevato)
+}
+
+// soluzione 2
+for (let n = 0; Math.pow(2, n) < 1000; n++) {
+    console.log(Math.pow(2, n))
 }
